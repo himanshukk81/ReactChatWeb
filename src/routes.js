05f317f1b61305037d7667407ef797898e3badf4
himanshukk81 <himanshukk81@gmail.com>
2021-10-10 17:06:00 +0530

    chnages

---
+++ src/routes.js
@@ -0,0 +1,10 @@
+
+export const Routes = {
+    Transactions: { path: "/" },
+    Signin: { path: "/examples/sign-in" },
+    Signup: { path: "/examples/sign-up" },
+    ForgotPassword: { path: "/examples/forgot-password" },
+    ResetPassword: { path: "/examples/reset-password" },
+    NotFound: { path: "/examples/404" },
+    Invoice: { path: "/examples/invoice" }
+};
